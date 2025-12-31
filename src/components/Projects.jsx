64be709e5e874fb362a587dcd3ce31109541985f
yanvic <yanvictoce@gmail.com>
2025-12-31 02:21:35 -0300

    feat: projects card adjust

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,31 +1,42 @@
-import React from "react";
 import { translations } from "../translate.js";
 import "../styles/projects.css";
 
-  export default function ProjectCard({ title, desc, image, link, lang = "en"  }) {
-    const t = translations[lang] || translations["en"];
-    return (
-      <article className="project-card">
-      <div
-        className="thumb"
-        style={{
-          backgroundImage:
-            "url(https://images.unsplash.com/photo-1498050108023-c5249f4df085)"
-        }}
-      />
-      <div className="info">
-        <h2>Projeto Test</h2>
-        <p>
-          Este projeto é para uso de senhas e etc. Exemplofkdfkadsfjbsdj
-          fbsdbfjbsdkjfdfjbjdsbbsdjbfbd
-          sajkfbsdjkbfjasbjfbasjkdbfbasjkdbfjsabkdjkfbsbfjksbadl.
-        </p>
-        <a href={link} target="_blank">View project</a>
-      </div>
-    </article>    
-    );
-  }
-  
+export default function Projects({ lang = "en" }) {
+  const t = translations[lang] || translations["pt"];
+  const projects = Array.isArray(t.projects_card ) ? t.projects_card : [];
 
+  return (
+    <div className="projects-container">
+      {projects.map((project, index) => (
+        <ProjectCard
+          key={index}
+          title={project.title}
+          image={project.image}
+          link={project.link}
+          desc={project.desc}
+          view={t.viewProject}
+        />
+      ))}
+    </div>
+  );
+}
 
-  
+function ProjectCard({ title, desc, image, link, view }) {
+  return (
+    <article className="project-card">
+    <div
+      className="thumb"
+      style={{
+        backgroundImage: `url(${image})`
+      }}
+    />
+    <div className="info">
+      <h3 className="title">{title}</h3>
+      <p>{desc}</p>
+      <a href={link} target="_blank" rel="noopener noreferrer">
+        {view}
+      </a>
+    </div>
+  </article>
+  );
+}
