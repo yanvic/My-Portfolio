0f1208a217e8f09ce8cdbe1ec3929ff8b4b04f40
yanvic <yanvictoce@gmail.com>
2026-01-09 00:39:11 -0300

    refactor: change translate native astro pages

---
+++ src/i18n/translate.js
@@ -0,0 +1,68 @@
+export const translations = {
+  en: {
+    home: "Home",
+    projects: "Projects",
+    about: "About Me",
+    contact: "Contact",
+    blog: "Blog",
+    welcome: "Welcome to my portfolio",
+    description: "This is the main content...",
+    hello: "Hi, I'm Yan",
+    presentation: "Developer in Golang and Rust",
+    button_projects: "Projects Look",
+    viewProject: "View project",
+    projects_card: [
+      {
+        title: "Project 1",
+        desc: "Description of Project 1 with some placeholder text. hjfdsafjldshfjhsdjdfdsfdds",
+        image: "https://images.unsplash.com/photo-1498050108023-c5249f4df085",
+        link: "https://example.com/projeto1"
+      },
+      {
+        title: "Project 2",
+        desc: "Description of Project 2 with some placeholder text.  hjfdsafjldshfjhsdjdfdsfddsdsds",
+         image: "https://images.unsplash.com/photo-1498050108023-c5249f4df085",
+        link: "https://example.com/projeto1"
+      },
+      {
+        title: "Project 3",
+        desc: "Description of Project 3 with some placeholder text  hjfdsafjldshfjhsdjdfdsfdds.",
+        image: "https://images.unsplash.com/photo-1498050108023-c5249f4df085",
+        link: "https://example.com/projeto1"
+      },
+    ],
+  },
+  pt: {
+    home: "Início",
+    projects: "Projetos",
+    about: "Sobre mim",
+    contact: "Contato",
+    blog: "Blog",
+    welcome: "Bem-vindo ao meu portfólio",
+    description: "Este é o conteúdo principal...",
+    hello: "Oi, eu sou o Yan",
+    presentation: "Desenvolvedor de software com experiência no mercado financeiro, webscrapping e vídeo monitoramento. buscando perfomance e escalabilidade, utilizando testes da melhor maneira.",
+    button_projects: "Ver Projetos",
+    viewProject: "Ver projeto",
+    projects_card: [
+      {
+        title: "Projeto 1",
+        desc: "Descrição do Projeto 1 com algum texto de exemplo. teste sendo testado jfnsdjfnsdjlk",
+        image: "https://images.unsplash.com/photo-1498050108023-c5249f4df085",
+        link: "https://example.com/projeto1"
+      },
+      {
+        title: "Projeto 2",
+        desc: "Descrição do Projeto 2 com algum texto de exemplo.  hjfdsafjldshfjhsdjdfdsfdds",
+        image: "https://images.unsplash.com/photo-1498050108023-c5249f4df085",
+        link: "https://example.com/projeto1"
+      },
+      {
+        title: "Projeto 3",
+        desc: "Descrição do Projeto 3 com algum texto de exemplo  hjfdsafjldshfjhsdjdfdsfdds.",
+        image: "https://images.unsplash.com/photo-1498050108023-c5249f4df085",
+        link: "https://example.com/projeto1"
+      },
+    ],
+  },
+};
